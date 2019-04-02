3ed47534941b30182a6b903f374311b7d790428e
hicksaj2 <48571801+hicksaj2@users.noreply.github.com>
2019-04-02 16:53:20 -0400

    Add files via upload

---
+++ MovieDatabase.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class MovieDatabase {
+	
+	// Private variables -----------------------------------------------------
+	
+	// List to hold the current movies of the database
+	private ArrayList<Movie> movieList = new ArrayList<Movie>();
+	
+	// Constructors ----------------------------------------------------------
+	
+	// Constructor for a Movie Database that already has a list of movies
+	public MovieDatabase(ArrayList<Movie> movieList) {
+		this.movieList = movieList;
+	}
+	
+	// Constructor for a Movie Database from a text file
+	public MovieDatabase(String filePath) throws FileNotFoundException {
+		loadData(filePath);
+	}
+
+	// Default constructor for a Movie database
+	public MovieDatabase() {}
+	
+	// Methods ---------------------------------------------------------------
+	
+	
+	// Loads data from a file into a Movie database
+	private void loadData(String filePath) throws FileNotFoundException {
+		// Creating the file and scanner to read from the file (skips first line)
+		File file = new File(filePath);
+		Scanner sc = new Scanner(file);
+		sc.nextLine();
+		
+		while(sc.hasNextLine()) {
+			String curLine = sc.nextLine();
+			String[] splitted = curLine.split("\t");
+			movieList.add(new Movie(
+									Integer.parseInt(splitted[0]), 	// movieID
+									splitted[1],						// title
+									Integer.parseInt(splitted[2]),	// year
+									splitted[3]						// genre
+									));
+		}
+		sc.close();
+	}
+	
+	// Returns all movies in the database
+	public void getAllMovies() {
+		for(Movie m : movieList) {
+			System.out.println("hi");
+			System.out.println(m.toString());
+		}
+	}
+	
+	// Adds a movie to the database
+	public void addMovie(Movie m) {
+		movieList.add(m);
+	}
+	
+	// Returns a movie by its ID
+	public Movie getMovieById(int id) {
+		Movie mov = null;
+		for(Movie m : movieList) {
+			if (m.getMovieID() == id) {
+				mov = m;
+				break;
+			}
+		}
+		return mov;
+	}
+	
+	// Returns movies that have a keyword in its title
+	public ArrayList<Movie> getMoviesByTitle(String title) {
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		for(Movie m : movieList) {
+			if (m.getTitle().contains(title)) {
+				movies.add(m);
+			}
+		}
+		return movies;
+	}
+	
+}
