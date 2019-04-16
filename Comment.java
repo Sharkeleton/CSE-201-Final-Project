6b3e9d5439b06c9f6704f0e8084cfb5fd0ba040a
peterzcq <48735948+peterzcq@users.noreply.github.com>
2019-04-16 17:08:27 -0400

    Add files via upload

---
+++ Comment.java
@@ -0,0 +1,34 @@
+
+public class Comment {
+    
+	private  String username;
+	private String com ;
+	
+	
+	public Comment(String username,String comment) {
+		username = this.username;
+		comment = this.com;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getCom() {
+		return com;
+	}
+
+
+	public void setCom(String com) {
+		this.com = com;
+	}
+	
+	
+}
