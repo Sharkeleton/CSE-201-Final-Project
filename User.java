93d12289dcbfb0e9d6b9aafc798dcb64dfc86d29
hicksaj2 <48571801+hicksaj2@users.noreply.github.com>
2019-04-16 17:35:03 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class User {
 	
@@ -6,11 +9,17 @@ private String lastName;
 private String userName;
 private String password;
 
-	public User(String fN, String lN,String uN,String pS) {
+	public User(String fN, String lN,String uN,String pS) throws FileNotFoundException {
 		fN = this.firstName;
 		lN = this.lastName;
 		uN = this.userName;
 		pS = this.password;
+		loadUser(this);
+	}
+
+	private void loadUser(User user) throws FileNotFoundException {
+		Scanner writer = new Scanner(new File("Users.txt"));
+		writer.close();
 	}
 
 	public String getFirstName() {
@@ -41,7 +50,8 @@ private String password;
 		this.password = password;
 	}
 	
-	
-	
+	public void addComment(MovieDatabase m, int id, String com) {
+		m.getMovieById(id).addComment(com, this);
+	}
 
 }
