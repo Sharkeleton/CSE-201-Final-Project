b34990e831f5d1355cd2989dc0872dc65b481dcc
Sharkeleton <37220686+Sharkeleton@users.noreply.github.com>
2019-04-16 18:04:31 -0400

    Update Tester.java

--- javaCode/Tester.java
+++ javaCode/Tester.java
@@ -4,10 +4,15 @@ import java.util.Collections;
 public class Tester {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		Movie m = new Movie(1,"A",2000,"Action");
 		MovieDatabase movies = new MovieDatabase("Movie.txt");
-		MovieCompareByTitle a = new MovieCompareByTitle();
-		movies.sortMovies();
+		movies.addMovie(m);
 		movies.getAllMovies();
+		movies.getMovieById(2);
+		movies.getMoviesByKey("A", "title");
+		movies.getMoviesByKey("Action", "genre");
+		movies.getMoviesByKey("2000", "year");
+		movies.sortMovies();
 	}
 
 }
