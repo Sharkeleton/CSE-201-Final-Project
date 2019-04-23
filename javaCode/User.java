36249baa649529914754792b7a1e24e04a890ee6
hicksaj2 <48571801+hicksaj2@users.noreply.github.com>
2019-04-23 16:32:15 -0400

    Update User.java

--- javaCode/User.java
+++ javaCode/User.java
@@ -6,89 +6,83 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class User {
-	
-private String firstName;
-private String lastName;
-private String userName;
-private String password;
-private int myID;
-private boolean isMod = false;
-private static int userID = 1;
-private static String modPassword = "abc";
 
-// not implemented yet
-private FileWriter writer = new FileWriter("Users.txt");
-private BufferedWriter out = new BufferedWriter(writer);
-private Scanner in = new Scanner(new File("Users.txt"));
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private int myID;
+    private boolean isMod = false;
+    private static int userID = 1;
+    private static String modPassword = "abc";
 
-	public User(String fN, String lN,String uN,String pS, String modCheck) throws IOException {
-		this.firstName = fN;
-		this.lastName = lN;
-		this.userName = uN;
-		this.password = pS;
-		myID = userID;
-        ++ userID;
-		if (modCheck.equals(modPassword)) {
-		    isMod = true;
-		}
-		
-		loadUser();
-	}
-	
-	
+    public User(String fN, String lN, String uN, String pS, String modCheck) throws IOException {
+        this.firstName = fN;
+        this.lastName = lN;
+        this.userName = uN;
+        this.password = pS;
+        myID = userID;
+        ++userID;
+        if (modCheck.equals(modPassword)) {
+            isMod = true;
+        }
+
+        loadUser();
+    }
+
     private void loadUser() throws IOException {
         File log = new File("Users.txt");
-        PrintWriter out = new PrintWriter(new FileWriter(log , true));
+        PrintWriter out = new PrintWriter(new FileWriter(log, true));
         out.append(userID + "\t" + firstName + "\t" + lastName + "\t" + userName + "\t" + password + "\n");
         out.close();
     }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public void addComment(MovieDatabase m, int id, Comment com) {
-		m.getNeedApprovedComments().add(com);
-	}
-	
-	public void addMovie(MovieDatabase m, Movie mov) {
-	    m.getNeedApprovedMovies().add(mov);
-	}
-	
-	public boolean approveMovie(MovieDatabase m, int movieID) {
-	    if (!isMod) {
-	        return false;
-	    }
-	    // approving movie code
-	    m.addMovie(m.getNeedApprovedMovies().get(movieID));
-	    return true;
- 	}
-	
-   public boolean approveComment(MovieDatabase m, int commentID) {
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addComment(MovieDatabase m, int id, Comment com) {
+        m.getNeedApprovedComments().add(com);
+    }
+
+    public void addMovie(MovieDatabase m, Movie mov) {
+        m.getNeedApprovedMovies().add(mov);
+    }
+
+    public boolean approveMovie(MovieDatabase m, int movieID) {
+        if (!isMod) {
+            return false;
+        }
+        // approving movie code
+        m.addMovie(m.getNeedApprovedMovies().get(movieID));
+        return true;
+    }
+
+    public boolean approveComment(MovieDatabase m, int commentID) {
         if (!isMod) {
             return false;
         }
@@ -98,10 +92,10 @@ private Scanner in = new Scanner(new File("Users.txt"));
         return true;
     }
 
-	@Override
-	public String toString() {
-		return "User [firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName + ", password="
-				+ password + "]";
-	}
+    @Override
+    public String toString() {
+        return "User [firstName=" + firstName + ", lastName=" + lastName + ", userName=" + userName + ", password="
+                + password + "]";
+    }
 
 }
