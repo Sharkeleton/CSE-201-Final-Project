ce49f2d17284a45ff3712dabe985b836e6705eb7
hicksaj2 <48571801+hicksaj2@users.noreply.github.com>
2019-04-23 16:08:56 -0400

    Update User.java

--- javaCode/User.java
+++ javaCode/User.java
@@ -1,4 +1,4 @@
-
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,23 +11,37 @@ private String firstName;
 private String lastName;
 private String userName;
 private String password;
+private int myID;
+private boolean isMod = false;
 private static int userID = 1;
+private static String modPassword = "abc";
 
-	public User(String fN, String lN,String uN,String pS) throws IOException {
+// not implemented yet
+private FileWriter writer = new FileWriter("Users.txt");
+private BufferedWriter out = new BufferedWriter(writer);
+private Scanner in = new Scanner(new File("Users.txt"));
+
+	public User(String fN, String lN,String uN,String pS, String modCheck) throws IOException {
 		this.firstName = fN;
 		this.lastName = lN;
 		this.userName = uN;
 		this.password = pS;
+		myID = userID;
+        ++ userID;
+		if (modCheck.equals(modPassword)) {
+		    isMod = true;
+		}
+		
 		loadUser();
 	}
-
-	private void loadUser() throws IOException {
-		File log = new File("Users.txt");
-		PrintWriter out = new PrintWriter(new FileWriter(log , true));
-		out.append(userID + "\t" + firstName + "\t" + lastName + "\t" + userName + "\t" + password + "\n");
-		out.close();
-		++ userID;
-	}
+	
+	
+    private void loadUser() throws IOException {
+        File log = new File("Users.txt");
+        PrintWriter out = new PrintWriter(new FileWriter(log , true));
+        out.append(userID + "\t" + firstName + "\t" + lastName + "\t" + userName + "\t" + password + "\n");
+        out.close();
+    }
 
 	public String getFirstName() {
 		return firstName;
@@ -57,9 +71,30 @@ private static int userID = 1;
 		this.password = password;
 	}
 	
-	public void addComment(MovieDatabase m, int id, String com) {
-		m.getMovieById(id).addComment(com, this);
+	public void addComment(MovieDatabase m, int id, Comment com) {
+		m.getNeedApprovedComments().add(com);
 	}
+	
+	public void addMovie(MovieDatabase m, Movie mov) {
+	    m.getNeedApprovedMovies().add(mov);
+	}
+	
+	public boolean approveMovie(MovieDatabase m, int movieID) {
+	    if (!isMod) {
+	        return false;
+	    }
+	    // approving movie code
+	    m.addMovie(m.getNeedApprovedMovies().get(movieID));
+	    return true;
+ 	}
+	
+   public boolean approveComment(MovieDatabase m, int commentID) {
+        if (!isMod) {
+            return false;
+        }
+        // approving comment code
+        return true;
+    }
 
 	@Override
 	public String toString() {
